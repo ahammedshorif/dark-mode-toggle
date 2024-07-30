3543b013e0d88804bb052b3688940d94c201e4db
ahammedshorf <ahammedshorif20@gmail.com>
2024-07-30 18:45:10 +0600

    commit 4 add local storage

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inputEl = document.querySelector('.input');
 const bodyEl = document.querySelector('body');
 
-// inputEl.checked= false;
-// updateBody();
+inputEl.checked= JSON.parse(localStorage.getItem("mode"));
+updateBody();
 
 function updateBody(){
     if(inputEl.checked==true){
@@ -15,4 +15,11 @@ function updateBody(){
 
 inputEl.addEventListener("input", ()=>{
     updateBody();
+    updateLocalStorage();
 })
+
+// add local storage
+
+function updateLocalStorage(){
+    localStorage.setItem("mode",JSON.stringify(inputEl.checked));
+}
